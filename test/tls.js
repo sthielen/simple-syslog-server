10a9b37aae9fb21e6f37f0e397786e4221dab125
Damien Clark <damo.clarky@gmail.com>
2018-11-12 12:55:15 +1000

    Fixed testing data in mocha test suite (facility & severity were reversed)

--- test/tls.js
+++ test/tls.js
@@ -26,8 +26,8 @@ describe( 'given a TLS Syslog Server', () => {
 			info.address = null ;
 			info.family = null ;
 			var shouldRet = {
-				  facility: 7
-				, severity: 22
+				  facility: 22
+				, severity: 7
 				, tag: 'tag'
 				, time: new Date(time + ' ' + new Date().getFullYear())
 				, hostname: 'hostname'
