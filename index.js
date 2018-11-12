44015381166be4d3e0fc828ad49e04c70b719b71
Damien Clark <damo.clarky@gmail.com>
2018-11-12 13:49:44 +1000

    Removed use of ES6 class definitions (keep all ES5 for now)

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ function Syslogd(fn, opt) {
 	this.server = dgram.createSocket('udp4') ;
 }
 
-var proto = Syslogd.prototype ;
-
-proto.listen = function(port, cb) {
+Syslogd.prototype.listen = function(port, cb) {
 	var server = this.server ;
 	if (this.port) {
 		debug('server has binded to %s', port) ;
@@ -46,7 +44,7 @@ proto.listen = function(port, cb) {
 	return this ;
 } ;
 
-proto.close = function( callback ){
+Syslogd.prototype.close = function( callback ){
 	this.server.close( callback ) ;
 } ;
 
@@ -187,135 +185,131 @@ StreamService.prototype.close = function( callback ) {
 	this.server.close(callback) ;
 } ;
 
-class ConnectionState {
-	constructor( service, connection ){
-		this.service = service ;
-		this.info = {
-			address: connection.remoteAddress,
-			family: connection.remoteFamily,
-			port: connection.remotePort
-		} ;
-		this.frameParser = new FrameParser( ( frame ) => {
-			this.dispatch_message( frame ) ;
-		}) ;
-	}
-
-	more_data( buffer ) {
-		this.frameParser.feed( buffer ) ;
-	}
-
-	dispatch_message( frame ) {
-		let clientInfo = {
-			address: this.info.address,
-			family: this.info.family,
-			port: this.info.port,
-			size: frame.length
-		} ;
-		console.log(`raw:${frame}`) ;
-		let message = parser( frame, clientInfo ) ;
-		this.service.handler( message ) ;
-	}
-
-	closed(){
-		this.frameParser.done() ;
-	}
+function ConnectionState( service, connection ){
+	this.service = service ;
+	this.info = {
+		address: connection.remoteAddress,
+		family: connection.remoteFamily,
+		port: connection.remotePort
+	} ;
+	this.frameParser = new FrameParser( ( frame ) => {
+		this.dispatch_message( frame ) ;
+	}) ;
 }
+
+ConnectionState.prototype.more_data = function( buffer ) {
+	this.frameParser.feed( buffer ) ;
+} ;
+
+ConnectionState.prototype.dispatch_message = function( frame ) {
+	let clientInfo = {
+		address: this.info.address,
+		family: this.info.family,
+		port: this.info.port,
+		size: frame.length
+	} ;
+	console.log(`raw:${frame}`) ;
+	let message = parser( frame, clientInfo ) ;
+	this.service.handler( message ) ;
+} ;
+
+ConnectionState.prototype.closed = function(){
+	this.frameParser.done() ;
+} ;
 
 let FRAME_TYPE_UNKNOWN = 0 ;
 let FRAME_TYPE_NEWLINE = 1 ;
 let FRAME_TYPE_OCTET = 2 ;
 
-class FrameParser {
-	constructor( callback ){
-		this.buffer = Buffer.from( '' ) ;
-		this.callback = callback ;
-		this.frame_state = FRAME_TYPE_UNKNOWN ;
-	}
-
-	feed( data ){
-		this.buffer = Buffer.concat( [ this.buffer, data ] ) ;
-		this.check_framing() ;
-	}
-
-	done() {
-		if( this.buffer.length > 0 )
-			this.callback( this.buffer.toString() ) ;
-		
-		this.buffer = Buffer.from( '', 'UTF-8' ) ;
-	}
-
-	check_framing(){
-		let continue_digesting ;
-		do {
-			if (this.frame_state == FRAME_TYPE_UNKNOWN) 
-				continue_digesting = this.decide_on_frame_type() ;
-			 else if (this.frame_state == FRAME_TYPE_NEWLINE) 
-				continue_digesting = this.check_newline_framing() ;
-			 else if (this.frame_state == FRAME_TYPE_OCTET) 
-				continue_digesting = this.check_octet_frame() ;
-			 else 
-				throw 'Invalid frame state' ;
-			
-		}while( continue_digesting );
-	}
-
-	decide_on_frame_type() {
-		// do nothing if buffer is too short
-		if( this.buffer.length < 8 ) 
-			return false ;
-		
-		// shrink our check buffer
-		let check = this.buffer.slice( 0, 8 ) ;
-		// Do we have spaces?
-		let space = check.indexOf( ' ' ) ;
-		if( space == -1 ){
-			this.frame_state = FRAME_TYPE_NEWLINE ;
-			return true ;
-		}
-
-		// Check output if we can convert it to a number
-		let size = parseInt( check.slice( 0, space ), 10 ) ;
-		if( isNaN( size ) || size < 2 ) {
-			this.frame_state = FRAME_TYPE_NEWLINE ;
-			return true ;
-		}
-
-		// Octet framing
-		this.octets = size ;
-		this.frame_state = FRAME_TYPE_OCTET ;
-		this.buffer = this.buffer.slice( space + 1 ) ;
-		return true ;
-	}
-
-	check_newline_framing() {
-		let indexOfNewLine = this.buffer.indexOf( '\n' ) ;
-		if( indexOfNewLine == -1 )  return false ; 
-
-		const frame = this.buffer.slice( 0, indexOfNewLine ) ;
-		this.buffer = this.buffer.slice( indexOfNewLine + 1 ) ;
-
-		return this._emit_and_reset( frame ) ;
-	}
-
-	check_octet_frame() {
-		let size = this.octets ;
-		if( !size )  throw 'Not currently in octet strategy' ; 
-
-		if( this.buffer.length < size )  return false ; 
-
-		let frame = this.buffer.slice( 0, size ) ;
-		this.buffer = this.buffer.slice( size ) ;
-
-		return this._emit_and_reset( frame ) ;
-	}
-
-	_emit_and_reset( frame ){
-		this.callback( frame.toString('utf-8') ) ;
-
-		this.frame_state = FRAME_TYPE_UNKNOWN ;
-		return true ;
-	}
+function FrameParser(callback) {
+	this.buffer = Buffer.from( '' ) ;
+	this.callback = callback ;
+	this.frame_state = FRAME_TYPE_UNKNOWN ;
 }
+
+FrameParser.prototype.feed = function( data ){
+	this.buffer = Buffer.concat( [ this.buffer, data ] ) ;
+	this.check_framing() ;
+} ;
+
+FrameParser.prototype.done = function() {
+	if( this.buffer.length > 0 )
+		this.callback( this.buffer.toString() ) ;
+
+	this.buffer = Buffer.from( '', 'UTF-8' ) ;
+} ;
+
+FrameParser.prototype.check_framing = function(){
+	let continue_digesting ;
+	do {
+		if (this.frame_state == FRAME_TYPE_UNKNOWN)
+			continue_digesting = this.decide_on_frame_type() ;
+		 else if (this.frame_state == FRAME_TYPE_NEWLINE)
+			continue_digesting = this.check_newline_framing() ;
+		 else if (this.frame_state == FRAME_TYPE_OCTET)
+			continue_digesting = this.check_octet_frame() ;
+		 else
+			throw 'Invalid frame state' ;
+
+	}while( continue_digesting );
+} ;
+
+FrameParser.prototype.decide_on_frame_type = function() {
+	// do nothing if buffer is too short
+	if( this.buffer.length < 8 )
+		return false ;
+
+	// shrink our check buffer
+	let check = this.buffer.slice( 0, 8 ) ;
+	// Do we have spaces?
+	let space = check.indexOf( ' ' ) ;
+	if( space == -1 ){
+		this.frame_state = FRAME_TYPE_NEWLINE ;
+		return true ;
+	}
+
+	// Check output if we can convert it to a number
+	let size = parseInt( check.slice( 0, space ), 10 ) ;
+	if( isNaN( size ) || size < 2 ) {
+		this.frame_state = FRAME_TYPE_NEWLINE ;
+		return true ;
+	}
+
+	// Octet framing
+	this.octets = size ;
+	this.frame_state = FRAME_TYPE_OCTET ;
+	this.buffer = this.buffer.slice( space + 1 ) ;
+	return true ;
+} ;
+
+FrameParser.prototype.check_newline_framing = function() {
+	let indexOfNewLine = this.buffer.indexOf( '\n' ) ;
+	if( indexOfNewLine == -1 )  return false ;
+
+	const frame = this.buffer.slice( 0, indexOfNewLine ) ;
+	this.buffer = this.buffer.slice( indexOfNewLine + 1 ) ;
+
+	return this._emit_and_reset( frame ) ;
+} ;
+
+FrameParser.prototype.check_octet_frame = function() {
+	let size = this.octets ;
+	if( !size )  throw 'Not currently in octet strategy' ;
+
+	if( this.buffer.length < size )  return false ;
+
+	let frame = this.buffer.slice( 0, size ) ;
+	this.buffer = this.buffer.slice( size ) ;
+
+	return this._emit_and_reset( frame ) ;
+} ;
+
+FrameParser.prototype._emit_and_reset = function( frame ){
+	this.callback( frame.toString('utf-8') ) ;
+
+	this.frame_state = FRAME_TYPE_UNKNOWN ;
+	return true ;
+} ;
 
 exports.StreamService = SimpleStreamService ;
 exports.TLSStreamService = tlsFactory ;
