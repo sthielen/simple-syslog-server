067247d46d0524be5d39ebfe45a833b6295e24cd
Damien Clark <damo.clarky@gmail.com>
2018-11-17 16:48:22 +1000

    Removed redundant branching

--- src/parser.js
+++ src/parser.js
@@ -31,32 +31,31 @@ function parser(msg, rinfo) {
 			msg: msg
 		} ;
 	}
-	else {
-		var format = msg.substr(0, tagIndex) ;
-		var priIndex = format.indexOf('>') ;
-		var pri = format.substr(1, priIndex - 1) ;
-		pri = parsePRI(pri) ;
-		var lastSpaceIndex = format.lastIndexOf(' ') ;
-		var tag = format.substr(lastSpaceIndex + 1) ;
-		var last2SpaceIndex = format.lastIndexOf(' ', lastSpaceIndex - 1) ; // hostname cannot contain ' '
-		var hostname = format.substring(last2SpaceIndex + 1, lastSpaceIndex) ;
-		// timestamp is complex because don't know if it has year
-		var timestamp = format.substring(priIndex + 1, last2SpaceIndex) ;
-		timestamp = new Date(timestamp) ;
-		timestamp.setYear(new Date().getFullYear()) ; // fix year to now
-		return {
-			facility: pri[0]
-			, severity: pri[1]
-			, tag: tag
-			, timestamp: timestamp
-			, hostname: hostname
-			, address: rinfo.address
-			, family: rinfo.family
-			, port: rinfo.port
-			, size: rinfo.size
-			, msg: msg.substr(tagIndex + 2)
-		} ;
-	}
+
+	var format = msg.substr(0, tagIndex) ;
+	var priIndex = format.indexOf('>') ;
+	var pri = format.substr(1, priIndex - 1) ;
+	pri = parsePRI(pri) ;
+	var lastSpaceIndex = format.lastIndexOf(' ') ;
+	var tag = format.substr(lastSpaceIndex + 1) ;
+	var last2SpaceIndex = format.lastIndexOf(' ', lastSpaceIndex - 1) ; // hostname cannot contain ' '
+	var hostname = format.substring(last2SpaceIndex + 1, lastSpaceIndex) ;
+	// timestamp is complex because don't know if it has year
+	var timestamp = format.substring(priIndex + 1, last2SpaceIndex) ;
+	timestamp = new Date(timestamp) ;
+	timestamp.setYear(new Date().getFullYear()) ; // fix year to now
+	return {
+		facility: pri[0]
+		, severity: pri[1]
+		, tag: tag
+		, timestamp: timestamp
+		, hostname: hostname
+		, address: rinfo.address
+		, family: rinfo.family
+		, port: rinfo.port
+		, size: rinfo.size
+		, msg: msg.substr(tagIndex + 2)
+	} ;
 }
 
 module.exports = parser ;
